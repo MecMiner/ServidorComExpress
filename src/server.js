9017268464f4ffa89abb3d7df18e0340c27974bd
MecMiner <souza.jefersoncarlos@gmail.com>
2025-10-24 15:19:12 -0300

    Adição de Crud

--- src/server.js
+++ src/server.js
@@ -1,13 +1,54 @@
 import express from "express"
+import { Database } from "./databases.js"
+import {randomUUID} from "node:crypto"
 const app = express()
+app.use(express.json())
+
+const database = new Database()
 
 app.get('/', (req, res) => {
     res.send("Você está na página principal")
 })
 
-app.post('/', (req, res)=> {
-    res.send("Você está na página principal usando post")
+app.get('/usuario', (req, res) => {
+    console.log(req.params)
+    console.log(req.query)
+    const {id} = req.params
+    const data = database.select("usuario")
+    res.status(200).json(data)
 })
+//http://localhost:3333/usuario/elonmusk
+app.get('/usuario/:id', (req, res) => {
+    const {id} = req.params
+    const data = database.selectItem("usuario", id)
+    res.status(200).json(data)
+})
+
+app.delete('/usuario/:id', (req, res) => {
+    const {id} = req.params
+    const result = database.delete("usuario", id)
+    res.status(200).json(result)
+})
+
+app.put('/usuario/:id', (req, res) => {
+    const {nome, idade, email} = req.body
+    const {id} = req.params
+    const result = database.update("usuario", id, {id, nome, idade, email})
+    res.status(200).json(result)
+})
+
+
+
+app.post('/usuario', (req, res)=> {
+    const {nome, idade, email} = req.body
+    const id = randomUUID()
+    if(nome, idade, email){
+        database.insert("usuario", {id, nome, idade, email})
+        return res.status(201).send("Usuário Criado")   
+    }
+    return res.status(400).send("Informações inválidas")
+})
+
 
 
 app.listen(3333, () => {
